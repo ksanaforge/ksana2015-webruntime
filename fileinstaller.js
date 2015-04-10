c8f5511c60ffe5e1fe54d98a00319deec42e1d16
yapcheahshen <yapcheahshen@gmail.com>
2015-04-10 22:55:07 +0800

    seperate material ui

--- fileinstaller.js
+++ fileinstaller.js
@@ -4,150 +4,19 @@ var HtmlFS=require("./htmlfs");
 var html5fs=require("./html5fs");
 var CheckBrowser=require("./checkbrowser");
 var E=React.createElement;
-  
 
-var FileList = React.createClass({
-	getInitialState:function() {
-		return {downloading:false,progress:0};
-	},
-	updatable:function(f) {
-        var classes="btn btn-warning";
-        if (this.state.downloading) classes+=" disabled";
-		if (f.hasUpdate) return   E("button", {className: classes, 
-			"data-filename": f.filename, "data-url": f.url, 
-	            onClick: this.download
-	       }, "Update")
-		else return null;
-	},
-	showLocal:function(f) {
-        var classes="btn btn-danger";
-        if (this.state.downloading) classes+=" disabled";
-	  return E("tr", null, E("td", null, f.filename), 
-	      E("td", null), 
-	      E("td", {className: "pull-right"}, 
-	      this.updatable(f), E("button", {className: classes, 
-	               onClick: this.deleteFile, "data-filename": f.filename}, "Delete")
-	        
-	      )
-	  )
-	},  
-	showRemote:function(f) { 
-	  var classes="btn btn-warning";
-	  if (this.state.downloading) classes+=" disabled";
-	  return (E("tr", {"data-id": f.filename}, E("td", null, 
-	      f.filename), 
-	      E("td", null, f.desc), 
-	      E("td", null, 
-	      E("span", {"data-filename": f.filename, "data-url": f.url, 
-	            className: classes, 
-	            onClick: this.download}, "Download")
-	      )
-	  ));
-	},
-	showFile:function(f) {
-	//	return <span data-id={f.filename}>{f.url}</span>
-		return (f.ready)?this.showLocal(f):this.showRemote(f);
-	},
-	reloadDir:function() {
-		this.props.action("reload");
-	},
-	download:function(e) {
-		var url=e.target.dataset["url"];
-		var filename=e.target.dataset["filename"];
-		this.setState({downloading:true,progress:0,url:url});
-		this.userbreak=false;
-		html5fs.download(url,filename,function(){
-			this.reloadDir();
-			this.setState({downloading:false,progress:1});
-			},function(progress,total){
-				if (progress==0) {
-					this.setState({message:"total "+total})
-			 	}
-			 	this.setState({progress:progress});
-			 	//if user press abort return true
-			 	return this.userbreak;
-			}
-		,this);
-	},
-	deleteFile:function( e) {
-		var filename=e.target.attributes["data-filename"].value;
-		this.props.action("delete",filename);
-	},
-	allFilesReady:function(e) {
-		return this.props.files.every(function(f){ return f.ready});
-	},
-	dismiss:function() {
-		$(this.refs.dialog1.getDOMNode()).modal('hide');
-		this.props.action("dismiss");
-	},
-	abortdownload:function() {
-		this.userbreak=true;
-	},
-	showProgress:function() {
-	     if (this.state.downloading) {
-	      var progress=Math.round(this.state.progress*100);
-	      return (
-	      	E("div", null, 
-	      	"Downloading from ", this.state.url, 
-	      E("div", {key: "progress", className: "progress col-md-8"}, 
-	          E("div", {className: "progress-bar", role: "progressbar", 
-	              "aria-valuenow": progress, "aria-valuemin": "0", 
-	              "aria-valuemax": "100", style: {width: progress+"%"}}, 
-	            progress, "%"
-	          )
-	        ), 
-	        E("button", {onClick: this.abortdownload, 
-	        	className: "btn btn-danger col-md-4"}, "Abort")
-	        )
-	        );
-	      } else {
-	      		if ( this.allFilesReady() ) {
-	      			return E("button", {onClick: this.dismiss, className: "btn btn-success"}, "Ok")
-	      		} else return null;
-	      		
-	      }
-	},
-	showUsage:function() {
-		var percent=this.props.remainPercent;
-           return (E("div", null, E("span", {className: "pull-left"}, "Usage:"), E("div", {className: "progress"}, 
-		  E("div", {className: "progress-bar progress-bar-success progress-bar-striped", role: "progressbar", style: {width: percent+"%"}}, 
-		    	percent+"%"
-		  )
-		)));
-	},
-	render:function() {
-	  	return (
-		E("div", {ref: "dialog1", className: "modal fade", "data-backdrop": "static"}, 
-		    E("div", {className: "modal-dialog"}, 
-		      E("div", {className: "modal-content"}, 
-		        E("div", {className: "modal-header"}, 
-		          E("h4", {className: "modal-title"}, "File Installer")
-		        ), 
-		        E("div", {className: "modal-body"}, 
-		        	E("table", {className: "table"}, 
-		        	E("tbody", null, 
-		          	this.props.files.map(this.showFile)
-		          	)
-		          )
-		        ), 
-		        E("div", {className: "modal-footer"}, 
-		        	this.showUsage(), 
-		           this.showProgress()
-		        )
-		      )
-		    )
-		  )
-		);
-	},	
-	componentDidMount:function() {
-		$(this.refs.dialog1.getDOMNode()).modal('show');
-	}
-});
+
+var FileList=null;
+
 /*TODO kdb check version*/
 var Filemanager = React.createClass({
 	getInitialState:function() {
 		var quota=this.getQuota();
 		return {browserReady:false,noupdate:true,	requestQuota:quota,remain:0};
+	},
+	componentWillMount:function(){
+	  if (ksanagap.bootopts.material) FileList=require("./filelist_mui");
+	  else FileList=require("./filelist");
 	},
 	getQuota:function() {
 		var q=this.props.quota||"128M";
@@ -215,14 +84,19 @@ var Filemanager = React.createClass({
 	},  
 	onBrowserOk:function() {
 	  this.totalDownloadSize();
+
 	}, 
 	dismiss:function() {
 		this.props.onReady(this.state.usage,this.state.quota);
+		
 		setTimeout(function(){
-			var modalin=$(".modal.in");
-			if (modalin.modal) modalin.modal('hide');
+			if (typeof $ !== "undefined") {
+				var modalin=$(".modal.in");
+				if (modalin.modal) modalin.modal('hide');				
+			}
 		},500);
 	}, 
+
 	totalDownloadSize:function() {
 		var files=this.missingKdb();
 		var taskqueue=[],totalsize=0;
